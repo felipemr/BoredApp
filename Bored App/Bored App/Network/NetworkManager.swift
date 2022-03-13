d74c3e4d3ab8228d6fc575aac8297e5e4e6c63c5
FelipeMR94 <felipemr94@outlook.com>
2022-03-13 15:23:30 -0300

    Network Manager creation and Random fetch of Bored API

---
+++ Bored App/Bored App/Network/NetworkManager.swift
@@ -0,0 +1,60 @@
+//
+//  NetworkManager.swift
+//  Bored App
+//
+//  Created by Felipe Marques Ramos on 3/13/22.
+//
+
+import Foundation
+import UIKit
+
+enum ErrorType: Error {
+    case invalidURL
+    case unableToComplete
+    case invalidResponse
+    case invalidData
+}
+
+class NetworkManager: NSObject {
+
+    static let baseURL = "https://www.boredapi.com/api/activity/"
+
+    static let shared = NetworkManager()
+    private let cache = NSCache<NSString, UIImage>()
+
+    private override init() {}
+
+    func forceSecureURL(url: String) -> String{
+        if url.contains("http://"){
+            return url.replacingOccurrences(of: "http://", with: "https://")
+        }
+        return url
+    }
+
+    func getData(for urlString: String, completed: @escaping (Result<Data, ErrorType>) -> Void) {
+        guard let url = URL(string: urlString) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+
+        URLSession.shared.dataTask(with: URLRequest(url: url)) { data, response, error in
+
+            if let _ =  error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+
+            completed(.success(data))
+        }.resume()
+    }
+}
