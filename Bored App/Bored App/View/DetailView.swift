d18936cba375cf600c20d36d059164e10f235e3f
FelipeMR94 <felipemr94@outlook.com>
2022-03-13 23:44:12 -0300

    Very Basic Detail View UI

--- Bored App/Bored App/View/DetailView.swift
+++ Bored App/Bored App/View/DetailView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct DetailView: View {
+
+    var activity: Activity
+
     var body: some View {
-        Image(systemName: "gamecontroller")
+        VStack{
+            Text(activity.activity)
+                .font(.largeTitle)
+            Text(activity.type)
+                .font(.headline)
+            Text(activity.key)
+                .font(.footnote)
+            Text("\(activity.participants ?? 1)")
+            Text("\(activity.price ?? 1)")
+            Text(activity.link ?? "")
+            Text("\(activity.accessibility ?? 1)")
+
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(activity: Activity(activity: "Nice", type: "Recreational", participants: 1, price: 0.5, link: "Google.com", key: "00000", accessibility: 0.1))
     }
 }
